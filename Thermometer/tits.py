3dabae902faa5183623f69891b6c922a696272e2
chadnewham <32149188+chadnewham@users.noreply.github.com>
2017-09-21 12:52:54 -0400

    Add files via upload

---
+++ Thermometer/tits.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+from flask import Flask, render_template, request
+import sqlite3 as sql
+import time
+import json
+import gviz_api
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/clear')
+def clearDb():
+    con = sql.connect('temp.db')
+    cur = con.cursor()
+    cur.execute("DELETE FROM readings")
+    con.commit()
+    msg = "Contents deleted"
+    return render_template('delete_all.html', msg = msg)
+    con.close()
+
+@app.route('/view', methods = ['POST', 'GET'])
+def viewData():
+    if request.method == 'GET':
+    	con = sql.connect('temp.db')
+	cur = con.cursor()
+    	cur.execute("SELECT * FROM readings")
+    	vals = cur.fetchall()
+	json_data = json.dumps(vals)
+	#description = [("temp", "number"), ("time", "number")]
+	#data_table = gviz_api.DataTable(description)
+	#data_table.LoadData(json_data)
+    	return render_template('shit.html', json_data = json_data)
+    	con.close()	
+
+@app.route('/home', methods = ['POST', 'GET'])
+def hello():
+    if request.method == 'POST':
+    	try:
+	    val = request.args.get('temp')
+	    tm = time.time()
+	    with sql.connect('temp.db') as con:
+                cur = con.cursor()
+    	        cur.execute("INSERT INTO readings(temp,time) VALUES (?,?)",(tm, val))
+	        con.commit()
+	        msg = "Added"
+	except:
+	    con.rollback()
+            msg = "Error"
+        finally:
+    	    return ('', 200)
+	    con.close()
+	    
+
+app.run (host='0.0.0.0')
+
